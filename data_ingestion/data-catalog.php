fb0143634121f4f915f6a3db3ee5bf67a74667f9
RelativisticMechanic <cyclicintegral@gmail.com>
2023-07-13 15:06:30 +0530

    Add modal view to the catalog, much better

--- data_ingestion/data-catalog.php
+++ data_ingestion/data-catalog.php
@@ -15,152 +15,186 @@
 <body>
     <div class="container">
         <h2 class="text-muted"><b>List of datasets uploaded to Apache&reg; Druid</b></h2>
-            <p>
-                <a class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#server_info" aria-expanded="false"
-                    aria-controls="server_info">
-                    Show Server Information
-                </a>
-            </p>
+        <p>
+            <a class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#server_info" aria-expanded="false"
+                aria-controls="server_info">
+                Show Server Information
+            </a>
+        </p>
 
-            <div class="collapse" id="server_info">
-                <table>
-                    <tr>
-                        <td>Running on</td>
-                        <td><span class="text-muted">
-                                <?php echo (php_uname('s') . '-' . php_uname('r') . '-' . php_uname('m')) ?>
-                            </span></td>
-                    </tr>
-                    <?php
-                    ini_set("allow_url_fopen", 1);
-                    $content = @file_get_contents('http://localhost:8888/status');
-                    if ($content == FALSE) {
-                        echo '<tr><td>Druid Status</td><td style="color:red;font-weight:bold;">NOT OK [USING CACHED CATALOG]</td></tr>';
+        <div class="collapse" id="server_info">
+            <table>
+                <tr>
+                    <td>Running on</td>
+                    <td><span class="text-muted">
+                            <?php echo (php_uname('s') . '-' . php_uname('r') . '-' . php_uname('m')) ?>
+                        </span></td>
+                </tr>
+                <?php
+                ini_set("allow_url_fopen", 1);
+                $content = @file_get_contents('http://localhost:8888/status');
+                if ($content == FALSE) {
+                    echo '<tr><td>Druid Status</td><td style="color:red;font-weight:bold;">NOT OK [USING CACHED CATALOG]</td></tr>';
+                } else {
+                    $obj = json_decode($content);
+                    $maxMem = ceil($obj->memory->maxMemory / (1024 * 1024));
+                    $freeMem = ceil($obj->memory->freeMemory / (1024 * 1024));
+                    $percentFree = ceil(($freeMem / $maxMem) * 100.0);
+                    echo '<tr><td>Druid Status</td><td style="color:green;font-weight:bold;">OK</td></tr>';
+                    echo "<tr><td>Druid version</td><td>" . $obj->version . "</td><tr>";
+                    echo "<tr><td>Max Memory</td><td>" . $maxMem . " MB" . '</td></tr>';
+                    echo "<tr><td>Free Memory</td><td>" . $freeMem . " MB (" . $percentFree . '%)' . '</td></tr>';
+                }
+                ?>
+            </table>
+        </div>
+
+        <div class="py-2" style="float:right;">
+            <label class="text-muted">Filter:</label>
+            <input type="text" id="search_table_name" onkeyup="searchTable()" placeholder="Filter by Name..">
+        </div>
+        <?php
+        // Function to sanitize the cell data
+        function sanitize($data)
+        {
+            return htmlspecialchars(trim($data));
+        }
+
+        // Parse the SDG Flag
+        function parse_sdgs($sdg_flags)
+        {
+            $SDG_URLS = array(
+                "",
+                "no-poverty",
+                "zero-hunger",
+                "good-health-and-well-being",
+                "quality-education",
+                "gender-equality",
+                "clean-water-and-sanitation",
+                "affordable-and-clean-energy",
+                "decent-work-and-economic-growth",
+                "industry-innovation-and-infrastructure",
+                "reduced-inequalities",
+                "sustainable-cities-and-communities",
+                "responsible-consumption-and-production",
+                "climate-action",
+                "life-below-water",
+                "life-on-land",
+                "peace-justice-and-strong-institutions",
+                "partnership-for-the-goals"
+            );
+            $sdg_out = "";
+            for ($i = 1; $i < 18; $i++) {
+                if ($sdg_flags & (1 << $i)) {
+                    $sdg_out .= '<a href="http://kdl.iiitb.ac.in/sdg-' . (string) $i . '-' . $SDG_URLS[$i] . '"> SDG ' . (string) $i . "</a>, ";
+                }
+            }
+            if ($sdg_out == "")
+                $sdg_out = 'None';
+            return substr($sdg_out, 0, -2);
+        }
+
+        chdir(dirname(__FILE__));
+
+        // Read the CSV file
+        $file = 'dataset-catalog.csv';
+
+        shell_exec("/usr/bin/python3 fetch_catalog.py " . $file);
+
+        $csvData = file_get_contents($file);
+        $lines = explode(PHP_EOL, $csvData);
+
+        // Generate the table
+        $table = '<table id="dataset-catalog" class="mx-auto w-100">';
+        $table .= '<tr>';
+        $table .= '<th>S.No</th>';
+        $table .= '<th>Name</th>';
+        $table .= '<th>Year</th>';
+        $table .= '<th>Dataset Description (optional)</th>';
+        $table .= '<th>Relevant SDGs</th>';
+        $table .= '<th>Filename</th>';
+        $table .= '<th>Uploaded On</th>';
+        $table .= '</tr>';
+
+        // Process each row
+        for ($i = 1; $i < count($lines); $i++) {
+            $cells = str_getcsv($lines[$i]);
+
+            // Only process rows with data
+            if (!empty($cells[0])) {
+                // Display the data rows
+                $table .= '<tr>';
+                // Serial number column
+                $table .= '<td>' . $i . '</td>';
+
+                for ($k = 0; $k < count($cells); $k++) {
+                    $cellData = sanitize($cells[$k]);
+
+                    // Remove surrounding quotes if present
+                    if (preg_match('/^"(.*)"$/s', $cellData, $matches)) {
+                        $cellData = $matches[1];
+                    }
+
+                    // Check if the column is "File Name"
+                    if ($k == 4) {
+                        $link = 'fetch.php?datasetName=' . urlencode($cellData);
+                        $table .= '<td><a onclick="setModalTitle(\'' . $cellData . '\'); fetch100Rows(\'' . $link . '\');" href="#data_fetch" data-bs-toggle="modal" data-target="#data_fetch">' . $cellData . '</a></td>';
+                    } else if ($k == 3) {
+                        // Parse SDGs
+                        $sdgs = parse_sdgs((int) $cellData);
+                        $table .= '<td>' . $sdgs . '</td>';
                     } else {
-                        $obj = json_decode($content);
-                        $maxMem =  ceil($obj->memory->maxMemory / (1024 * 1024));
-                        $freeMem = ceil($obj->memory->freeMemory / (1024 * 1024));
-                        $percentFree = ceil(($freeMem / $maxMem)*100.0);
-                        echo '<tr><td>Druid Status</td><td style="color:green;font-weight:bold;">OK</td></tr>';
-                        echo "<tr><td>Druid version</td><td>" . $obj->version . "</td><tr>";
-                        echo "<tr><td>Max Memory</td><td>" . $maxMem . " MB" . '</td></tr>';
-                        echo "<tr><td>Free Memory</td><td>" . $freeMem . " MB (" . $percentFree . '%)' . '</td></tr>';
-                    }
-                    ?>
-                </table>
-            </div>
-
-            <div class="py-2" style="float:right;">
-                <label class="text-muted">Filter:</label>
-                <input type="text" id="search_table_name" onkeyup="searchTable()" placeholder="Filter by Name..">
-            </div>
-            <?php
-            // Function to sanitize the cell data
-            function sanitize($data)
-            {
-                return htmlspecialchars(trim($data));
-            }
-
-            // Parse the SDG Flag
-            function parse_sdgs($sdg_flags)
-            {
-                $SDG_URLS = array(
-                    "",
-                    "no-poverty",
-                    "zero-hunger",
-                    "good-health-and-well-being",
-                    "quality-education",
-                    "gender-equality",
-                    "clean-water-and-sanitation",
-                    "affordable-and-clean-energy",
-                    "decent-work-and-economic-growth",
-                    "industry-innovation-and-infrastructure",
-                    "reduced-inequalities",
-                    "sustainable-cities-and-communities",
-                    "responsible-consumption-and-production",
-                    "climate-action",
-                    "life-below-water",
-                    "life-on-land",
-                    "peace-justice-and-strong-institutions",
-                    "partnership-for-the-goals"
-                );
-                $sdg_out = "";
-                for ($i = 1; $i < 18; $i++) {
-                    if ($sdg_flags & (1 << $i)) {
-                        $sdg_out .= '<a href="http://kdl.iiitb.ac.in/sdg-' . (string) $i . '-' . $SDG_URLS[$i] . '"> SDG ' . (string) $i . "</a>, ";
+                        $table .= '<td>' . $cellData . '</td>';
                     }
                 }
-                if ($sdg_out == "")
-                    $sdg_out = 'None';
-                return substr($sdg_out, 0, -2);
+
+                $table .= '</tr>';
             }
+        }
 
-            chdir(dirname(__FILE__));
+        $table .= '</table>';
 
-            // Read the CSV file
-            $file = 'dataset-catalog.csv';
+        echo $table;
+        ?>
+    </div>
 
-            shell_exec("/usr/bin/python3 fetch_catalog.py " . $file);
-
-            $csvData = file_get_contents($file);
-            $lines = explode(PHP_EOL, $csvData);
-
-            // Generate the table
-            $table = '<table id="dataset-catalog" class="mx-auto w-100">';
-            $table .= '<tr>';
-            $table .= '<th>S.No</th>';
-            $table .= '<th>Name</th>';
-            $table .= '<th>Year</th>';
-            $table .= '<th>Dataset Description (optional)</th>';
-            $table .= '<th>Relevant SDGs</th>';
-            $table .= '<th>Filename</th>';
-            $table .= '<th>Uploaded On</th>';
-            $table .= '</tr>';
-
-            // Process each row
-            for ($i = 1; $i < count($lines); $i++) {
-                $cells = str_getcsv($lines[$i]);
-
-                // Only process rows with data
-                if (!empty($cells[0])) {
-                    // Display the data rows
-                    $table .= '<tr>';
-                    // Serial number column
-                    $table .= '<td>' . $i . '</td>';
-
-                    for ($k = 0; $k < count($cells); $k++) {
-                        $cellData = sanitize($cells[$k]);
-
-                        // Remove surrounding quotes if present
-                        if (preg_match('/^"(.*)"$/s', $cellData, $matches)) {
-                            $cellData = $matches[1];
-                        }
-
-                        // Check if the column is "File Name"
-                        if ($k == 4) {
-                            $link = 'fetch.php?datasetName=' . urlencode($cellData);
-                            $table .= '<td><a href="' . $link . '">' . $cellData . '</a></td>';
-                        } else if ($k == 3) {
-                            // Parse SDGs
-                            $sdgs = parse_sdgs((int) $cellData);
-                            $table .= '<td>' . $sdgs . '</td>';
-                        } else {
-                            $table .= '<td>' . $cellData . '</td>';
-                        }
-                    }
-
-                    $table .= '</tr>';
-                }
-            }
-
-            $table .= '</table>';
-
-            echo $table;
-            ?>
+    <div class="modal fade" id="data_fetch" tabindex="-1" role="dialog" aria-labelledby="data_fetch_title"
+        aria-hidden="true">
+        <div class="modal-dialog w-75" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="data_fetch_title"></h5>
+                    <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal" aria-label="Close">
+                        <span class="fa fa-x"></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="table-responsive" id="data_fetch_table" style="height:400px;">
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div id="n_columns"></div>
+                </div>
+            </div>
+        </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://cdn.usebootstrap.com/bootstrap/5.0.1/js/bootstrap.bundle.min.js"></script>
     <script>
+        function setModalTitle(title) {
+            $("#data_fetch_title").html(title);
+        }
+        function fetch100Rows(fetch_url) {
+            $.ajax({
+                url: fetch_url,
+                type: 'GET',
+                success: (data) => {
+                    $("#data_fetch_table").html(data);
+                    $("#n_columns").html('Columns: ' + document.getElementById('fetch_php_table').rows[0].cells.length);
+                }
+            });
+        }
         const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
         const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
--- data_ingestion/fetch.php
+++ data_ingestion/fetch.php
@@ -1,71 +1,52 @@
-<!DOCTYPE html>
-<html>
+<?php
+// Function to sanitize the cell data
+function sanitize($data)
+{
+    return htmlspecialchars(trim($data));
+}
 
-<head>
-    <title>Data Fetch</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="styles/google-font.css" rel="stylesheet" type="text/css" />
-    <link href="styles/styles.css" rel="stylesheet" type="text/css" />
-    <link href="styles/tables.css" rel=stylesheet type="text/css" />
+// Check if the datasetName parameter is present in the URL
+if (isset($_GET['datasetName'])) {
+    // Get the dataset name from the URL parameter
+    $datasetName = $_GET['datasetName'];
 
-</head>
+    // Call the Python script using system command
+    $command = '/usr/bin/python3 fetch_rows.py ' . escapeshellarg($datasetName);
+    $output = shell_exec($command);
 
-<body>
-    <div class="container-fluid gfont">
-        <?php
-        // Function to sanitize the cell data
-        function sanitize($data)
-        {
-            return htmlspecialchars(trim($data));
+    // Decode the JSON response
+    $data = json_decode($output, true);
+
+    // Generate the table
+    if (!empty($data)) {
+        $table = '<table class="table mx-auto" id="fetch_php_table">';
+        $table .= '<tr>';
+
+        // Generate table headers dynamically based on the JSON keys
+        foreach ($data[0] as $key => $value) {
+            $table .= '<th>' . sanitize($key) . '</th>';
         }
 
-        // Check if the datasetName parameter is present in the URL
-        if (isset($_GET['datasetName'])) {
-            // Get the dataset name from the URL parameter
-            $datasetName = $_GET['datasetName'];
+        $table .= '</tr>';
 
-            // Call the Python script using system command
-            $command = '/usr/bin/python3 fetch_rows.py ' . escapeshellarg($datasetName);
-            $output = shell_exec($command);
+        // Display the first 100 rows
+        for ($i = 0; $i < min(100, count($data)); $i++) {
+            $table .= '<tr>';
 
-            // Decode the JSON response
-            $data = json_decode($output, true);
-
-            // Generate the table
-            if (!empty($data)) {
-                $table = '<table class="mx-auto">';
-                $table .= '<tr>';
-
-                // Generate table headers dynamically based on the JSON keys
-                foreach ($data[0] as $key => $value) {
-                    $table .= '<th>' . sanitize($key) . '</th>';
-                }
-
-                $table .= '</tr>';
-
-                // Display the first 100 rows
-                for ($i = 0; $i < min(100, count($data)); $i++) {
-                    $table .= '<tr>';
-
-                    foreach ($data[$i] as $value) {
-                        $table .= '<td>' . sanitize($value) . '</td>';
-                    }
-
-                    $table .= '</tr>';
-                }
-
-                $table .= '</table>';
-            } else {
-                $table = 'No data available. Druid is perhaps not running on the server. If you are a server administrator reading this, please initialize Druid.';
+            foreach ($data[$i] as $value) {
+                $table .= '<td>' . sanitize($value) . '</td>';
             }
 
-            echo $table;
-        } else {
-            echo 'Invalid request.';
+            $table .= '</tr>';
         }
-        ?>
-    </div>
-</body>
 
-</html>
+        $table .= '</table>';
+    } else {
+        $table = 'No data available. Druid is perhaps not running on the server. If you are a server administrator reading this, please initialize Druid.';
+    }
+
+    echo $table;
+} else {
+    echo 'Invalid request.';
+}
+?>
